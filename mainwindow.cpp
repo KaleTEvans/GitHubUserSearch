3639e8259aa2e8d775b26b7863b35b53932dec82
Kale Evans <kaletevans@gmail.com>
2022-02-27 11:19:54 -0600

    fixed bug causing application crash

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QInputDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,10 +13,128 @@ MainWindow::MainWindow(QWidget *parent)
 {
     setWindowIcon(QIcon(":/Images/GitHub.png"));
     ui->setupUi(this);
+    netManager = new QNetworkAccessManager(this);
+    netReply = nullptr;
+    repoReply = nullptr;
+    img = new QPixmap();
+}
+
+void MainWindow::clearValues()
+{
+    ui->labelImage->clear();
+    ui->labelName->clear();
+    ui->labelUser->clear();
+    ui->labelRepo->clear();
+    ui->lineEditRepo->clear();
+    ui->textEditBio->clear();
+    ui->labelFollowers->clear();
+    ui->labelFollowing->clear();
+    ui->labelAccount->clear();
+    dataBuffer.clear();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_searchButton_clicked()
+{
+    auto username = QInputDialog::getText(this, "Github Username", "Enter a GitHub Username");
+    if (!username.isEmpty()) {
+        // clear all of the data from the previous search
+        clearValues();
+        // these are the api calls, one to the user data, and one to the users repos
+        QNetworkRequest req{QUrl(QString("https://api.github.com/users/%1").arg(username))};
+        //QNetworkRequest repoReq{QUrl(QString("https://api.github.com/users/%1/repos").arg(username))};
+
+        // connect and read the data for the user
+        netReply = netManager->get(req);
+        connect(netReply, &QNetworkReply::readyRead, this, &MainWindow::readData);
+        connect(netReply, &QNetworkReply::finished, this, &MainWindow::finishReading);
+
+        // connect and read the data for the user's repos
+        //repoReply = netManager->get(repoReq);
+        //connect(repoReply, &QNetworkReply::readyRead, this, &MainWindow::readDataForRepo);
+        //connect(repoReply, &QNetworkReply::finished, this, &MainWindow::finishedGettingRepos);
+    }
+}
+
+// Read data custom slot
+void MainWindow::readData()
+{
+    dataBuffer.append(netReply->readAll());
+}
+
+// Finished reading custom slot
+void MainWindow::finishReading()
+{
+    if (netReply->error() != QNetworkReply::NoError) {
+        // output the netReply error with qDebug
+        qDebug() << "Error: " << netReply->errorString();
+        QMessageBox::warning(this, "Error", QString("Request[Error] : %1"));
+    }  else {
+        // Convert the data from a JSON doc to a JSON object
+        QJsonObject userJsonInfo = QJsonDocument::fromJson(dataBuffer).object();
+
+        // Set username
+        QString login = userJsonInfo.value("login").toString();
+        ui->lineEditUsername->setText(login);
+
+        // Set display name
+        QString name = userJsonInfo.value("name").toString();
+        ui->lineEditName->setText(name);
+
+        // Set bio
+        auto bio = userJsonInfo.value("bio").toString();
+        ui->textEditBio->setText(bio);
+
+        // Set follower and following count
+//        auto follower = userJsonInfo.value("followers").toString();
+//        auto following = userJsonInfo.value("following").toString();
+//        ui->lineEditFollowers->setText(follower);
+//        ui->lineEditFollowing->setText(following);
+
+        // Set account type
+//        QString type = userJsonInfo.value("type").toString();
+//        ui->lineEditAccount->setText(type);
+
+        // Set picture
+//        auto picLink = userJsonInfo.value("avatar_url").toString();
+//        QNetworkRequest link{QUrl(picLink)};
+//        netReply = netManager->get(link);
+//        connect(netReply, &QNetworkReply::finished, this, &MainWindow::setUserImage);
+//        dataBuffer.clear();
+    }
+}
+
+// Set the user image
+//void MainWindow::setUserImage()
+//{
+//    img->loadFromData(netReply->readAll());
+//    QPixmap temp = img->scaled(ui->labelImage->size());
+//    ui->labelImage->setPixmap(temp);
+//}
+
+//void MainWindow::readDataForRepo()
+//{
+//    dataBuffer.append(repoReply->readAll());
+//}
+
+//void MainWindow::finishedGettingRepos()
+//{
+//    if(repoReply->error() != QNetworkReply::NoError){
+//        qDebug() << "Error Getting List of Repo: " << netReply->errorString();
+//        QMessageBox::warning(this,"Error",QString("Request[Error] : %1").arg(netReply->errorString()));
+//    }else{
+//        QJsonArray repoInfo = QJsonDocument::fromJson(dataBuffer).array();
+//        ui->lineEditRepo->setValue(repoInfo.size());
+//        for(int i{0}; i < ui->lineEditRepo->value(); ++i){
+//            auto repo = repoInfo.at(i).toObject();
+//            QString repoName = repo.value("name").toString();
+//            ui->textEditRepo->addItem(repoName);
+//        }
+//    }
+//}
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QByteArray>
+#include <QPixmap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,9 +19,23 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+    void clearValues();
     ~MainWindow();
+
+private slots:
+    void on_searchButton_clicked();
+    void readData();
+    //void readDataForRepo();
+    //void finishedGettingRepos();
+    void finishReading();
+    //void setUserImage();
 
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *netManager;
+    QNetworkReply *netReply;
+    QNetworkReply *repoReply;
+    QByteArray dataBuffer;
+    QPixmap *img;
 };
 #endif // MAINWINDOW_H
